fcdad7784eb49e52ab9dcf9bbd62e2e6397886d9
XzoRit <xzorit@hotmail.com>
2016-04-14 21:37:42 +0200

    save, load and printArchive are available

--- serialization/main.cpp
+++ serialization/main.cpp
@@ -1,10 +1,41 @@
 #include "GpsPosition.hpp"
 #include <boost/archive/text_oarchive.hpp>
 #include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/xml_oarchive.hpp>
+#include <boost/archive/xml_iarchive.hpp>
 #include <fstream>
 #include <iostream>
 #include <iterator>
 #include <cassert>
+
+template<class Archive>
+void save(const std::string& name, const GpsPosition& gps)
+{
+  std::ofstream oFile(name);
+  Archive ar(oFile);
+  ar << gps;
+}
+
+template<class Archive>
+GpsPosition load(const std::string& name)
+{
+  std::ifstream in(name);
+  GpsPosition newGps;
+  {
+    Archive i(in);
+    i >> newGps;
+  }
+  return newGps;
+}
+
+void printArchive(const std::string& name)
+{
+  std::ifstream in(name);
+  const std::string archiveData = std::string(std::istreambuf_iterator<char>(in),
+					      std::istreambuf_iterator<char>());
+  std::cout << "data in archive " << name << "\n";
+  std::cout << archiveData << '\n';
+}
 
 int main(int argc, char *argv[])
 {
@@ -12,30 +43,13 @@ int main(int argc, char *argv[])
   using boost::archive::text_iarchive;
   
   std::cout << "\nboost_serialization\n\n";
-    
-  std::ofstream out("ar");
+
   const GpsPosition gps(1, 2, 3.0f);
-  std::cout << "about to serialize gps = " << gps << '\n';
-  {
-    boost::archive::text_oarchive o(out);
-    o << gps;
-  }
-
-  {
-    std::ifstream in("ar");
-    const std::string archiveData = std::string(std::istreambuf_iterator<char>(in),
-						std::istreambuf_iterator<char>());
-    std::cout << "data in archive\n";
-    std::cout << archiveData << '\n';
-  }
-  std::ifstream in("ar");
-  GpsPosition newGps;
-  {
-    text_iarchive i(in);
-    i >> newGps;
-    std::cout << "read back from file newGps = " << newGps << '\n';
-  }
-
+  std::cout << "about to save gps = " << gps << '\n';
+  save<text_oarchive>("ar", gps);
+  printArchive("ar");
+  const GpsPosition newGps = load<text_iarchive>("ar");
+  std::cout << "loaded newGps = " << newGps << '\n';
   assert(gps == newGps);
   
   return 0;
