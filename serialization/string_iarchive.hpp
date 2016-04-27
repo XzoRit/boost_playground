ceddb2a92e189a733550e14e2667363a02256b4d
XzoRit <xzorit@hotmail.com>
2016-04-27 20:22:34 +0200

    added load hooks to string iarchive

--- serialization/string_iarchive.hpp
+++ serialization/string_iarchive.hpp
@@ -20,6 +20,113 @@ namespace xzr
 	, boost::archive::basic_text_iarchive<Archive>(flags)
       {
       }
+
+      void load_start(const char* name)
+      {
+      }
+      void load_end(const char* name)
+      {
+      }
+      
+      template<class T>
+      void load(T& t)
+      {
+	basic_text_iprimitive<std::istream>::load(t);
+      }
+
+      void load(bool& t)
+      {
+	basic_text_iprimitive<std::istream>::load(t);
+      }
+      void load(char& t)
+      {
+	basic_text_iprimitive<std::istream>::load(t);
+      }
+      void load(signed char& t)
+      {
+	basic_text_iprimitive<std::istream>::load(t);
+      }
+      void load(unsigned char& t)
+      {
+	basic_text_iprimitive<std::istream>::load(t);
+      }
+      void load(wchar_t& t)
+      {
+	basic_text_iprimitive<std::istream>::load(t);
+      }
+      void load(short& t)
+      {
+	basic_text_iprimitive<std::istream>::load(t);
+      }
+      void load(unsigned short& t)
+      {
+	basic_text_iprimitive<std::istream>::load(t);
+      }
+      void load(int& t)
+      {
+	basic_text_iprimitive<std::istream>::load(t);
+      }
+      void load(unsigned int& t)
+      {
+	basic_text_iprimitive<std::istream>::load(t);
+      }
+      void load(long& t)
+      {
+	basic_text_iprimitive<std::istream>::load(t);
+      }
+      void load(unsigned long& t)
+      {
+	basic_text_iprimitive<std::istream>::load(t);
+      }
+      void load(boost::long_long_type& t)
+      {
+	basic_text_iprimitive<std::istream>::load(t);
+      }
+      void load(boost::ulong_long_type& t)
+      {
+	basic_text_iprimitive<std::istream>::load(t);
+      }
+      void load(float& t)
+      {
+	basic_text_iprimitive<std::istream>::load(t);
+      }
+      void load(double& t)
+      {
+	basic_text_iprimitive<std::istream>::load(t);
+      }
+      void load(std::string& t)
+      {
+	basic_text_iprimitive<std::istream>::load(t);
+      }
+      void load(std::wstring& t)
+      {
+	basic_text_iprimitive<std::istream>::load(t);
+      }
+      
+      void load(boost::archive::version_type &t)
+      {
+	is >> t;
+      }
+      void load(boost::archive::object_id_type &t)
+      {
+	is >> t;
+      }
+      void load(boost::archive::class_id_type &t)
+      {
+	is >> t;
+      }
+      void load(boost::archive::class_id_optional_type &t)
+      {
+	is >> t;
+      }
+      void load(boost::archive::class_name_type &t)
+      {
+	is >> t;
+      }
+      void load(boost::archive::tracking_type &t)
+      {
+	is >> t;
+      }
     };
   
     class string_iarchive
