c3a4dc579b68d7cb3d740c4234a46bc2ab918fbb
XzoRit <xzorit@hotmail.com>
2016-08-10 14:34:31 +0200

    added test case for reserve more on filled vector

--- test/playground/main.cpp
+++ test/playground/main.cpp
@@ -39,6 +39,18 @@ struct reserve_vector_smaller : public init_vector
     }
     ~reserve_vector_smaller() {}
 };
+
+struct reserve_vector_bigger : public init_vector
+{
+    reserve_vector_bigger()
+	: bigger_capacity{v.capacity() * 3}
+	{
+	    v.reserve(bigger_capacity);
+	}
+    ~reserve_vector_bigger() {}
+    const std::size_t bigger_capacity{0};
+};
+
 BOOST_AUTO_TEST_SUITE(vector)
 
 BOOST_FIXTURE_TEST_CASE(size_and_capacity_after_init, init_vector)
@@ -63,6 +75,12 @@ BOOST_FIXTURE_TEST_CASE(size_and_capacity_not_changed_after_reserve_smaller, res
 {
     BOOST_TEST(v.size() == init_size);
     BOOST_TEST(v.capacity() >= init_size);
+}
+
+BOOST_FIXTURE_TEST_CASE(size_not_changing_but_capacity_after_reserve_bigger, reserve_vector_bigger)
+{
+    BOOST_TEST(v.size() == init_size);
+    BOOST_TEST(v.capacity() >= bigger_capacity);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
