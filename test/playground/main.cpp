861aba95274f68ac60f5fc7a608b3e0c6b2ee45e
XzoRit <xzorit@hotmail.com>
2016-08-10 17:12:26 +0200

    added messaged to debug fixture creation/destruction

--- test/playground/main.cpp
+++ test/playground/main.cpp
@@ -5,8 +5,14 @@
 
 struct init_vector
 {
-    init_vector() {}
-    ~init_vector() {}
+    init_vector()
+    {
+        BOOST_TEST_MESSAGE("init_vector");
+    }
+    ~init_vector()
+    {
+        BOOST_TEST_MESSAGE("~init_vector");
+    }
     const std::size_t init_size{5};
     std::vector<int> v = std::vector<int>(init_size);
 };
@@ -15,9 +21,13 @@ struct resize_vector_bigger : public init_vector
 {
     resize_vector_bigger()
     {
+        BOOST_TEST_MESSAGE("resize_vector_bigger");
         v.resize(bigger_size);
     }
-    ~resize_vector_bigger() {}
+    ~resize_vector_bigger()
+    {
+        BOOST_TEST_MESSAGE("~resize_vector_bigger");
+    }
     const std::size_t bigger_size{init_size * 2};
 };
 
@@ -25,9 +35,13 @@ struct resize_vector_smaller : public init_vector
 {
     resize_vector_smaller()
     {
+        BOOST_TEST_MESSAGE("resize_vector_smaller");
         v.resize(smaller_size);
     }
-    ~resize_vector_smaller() {}
+    ~resize_vector_smaller()
+    {
+        BOOST_TEST_MESSAGE("~resize_vector_smaller");
+    }
     const std::size_t smaller_size{init_size - init_size};
 };
 
@@ -35,29 +49,41 @@ struct reserve_vector_smaller : public init_vector
 {
     reserve_vector_smaller()
     {
+        BOOST_TEST_MESSAGE("resize_vector_smaller");
         v.reserve(init_size - init_size);
     }
-    ~reserve_vector_smaller() {}
+    ~reserve_vector_smaller()
+    {
+        BOOST_TEST_MESSAGE("~resize_vector_smaller");
+    }
 };
 
 struct reserve_vector_bigger : public init_vector
 {
     reserve_vector_bigger()
-	: bigger_capacity{v.capacity() * 3}
-	{
-	    v.reserve(bigger_capacity);
-	}
-    ~reserve_vector_bigger() {}
+        : bigger_capacity{v.capacity() * 3}
+    {
+        BOOST_TEST_MESSAGE("resize_vector_bigger");
+        v.reserve(bigger_capacity);
+    }
+    ~reserve_vector_bigger()
+    {
+        BOOST_TEST_MESSAGE("~resize_vector_bigger");
+    }
     const std::size_t bigger_capacity{0};
 };
 
 struct reserve_vector_smaller_again : public reserve_vector_bigger
 {
     reserve_vector_smaller_again()
-	{
-	    v.reserve(bigger_capacity - bigger_capacity);
-	}
-    ~reserve_vector_smaller_again() {}
+    {
+        BOOST_TEST_MESSAGE("reserve_vector_smaller_again");
+        v.reserve(bigger_capacity - bigger_capacity);
+    }
+    ~reserve_vector_smaller_again()
+    {
+        BOOST_TEST_MESSAGE("~reserve_vector_smaller_again");
+    }
 };
 
 BOOST_AUTO_TEST_SUITE(vector)
