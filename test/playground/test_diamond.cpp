4a811c00be9d57eaabcfd33876b7b1b9388ca9c0
XzoRit <xzorit@hotmail.com>
2016-08-23 14:03:33 +0200

    spaces are inserted in each line

--- test/playground/test_diamond.cpp
+++ test/playground/test_diamond.cpp
@@ -24,37 +24,50 @@ auto num_of_letters(char c)
 
 auto letters(int num)
 {
-    return "ABCDEFGHIJKLMNOPQRSTUVWXYZ"s.substr(0, num);
-}
-
-  auto append_reversed(const vector<string>& letters)
-{
-  auto reversed{vector<string>(next(rbegin(letters)), rend(letters))};
-  reversed.insert(begin(reversed), begin(letters), end(letters));
-  return reversed;
-}
-
-  auto double_letters(const std::string& letters)
-{
-    auto doubled{vector<string>{"A"}};
-    if(letters.size() == 1) return doubled;
-    for(auto&& c : string{next(begin(letters)), end(letters)})
+  const auto chars{"ABCDEFGHIJKLMNOPQRSTUVWXYZ"s.substr(0, num)};
+  auto seq{vector<string>{}};
+  for(auto&& c : chars)
     {
-      doubled.push_back(string(2, c));
+      seq.push_back(string{c});
     }
-    return doubled;
+  return seq;
 }
 
-  auto join(const vector<string>& lines)
-  {
+auto append_reversed(const vector<string>& letters)
+{
+    auto reversed{vector<string>(next(rbegin(letters)), rend(letters))};
+    reversed.insert(begin(reversed), begin(letters), end(letters));
+    return reversed;
+}
+
+auto insert_spaces(const vector<string>& lines)
+{
+    if(lines.size() == 1) return lines;
+    auto spaced_lines{vector<string>{}};
+    for(size_t i{0}, j{lines.size()}; i < lines.size(); ++i, --j)
+    {
+        spaced_lines.push_back(
+            string(j-1, ' ') +
+            lines[i][0] +
+            string(i, ' '));
+    }
+    for(size_t i{0}, j{lines.size()}; i < lines.size(); ++i, --j)
+    {
+        spaced_lines[i] = spaced_lines[i] + string{next(rbegin(spaced_lines[i])), rend(spaced_lines[i])};
+    }
+    return spaced_lines;
+}
+
+auto join(const vector<string>& lines)
+{
     return accumulate(begin(lines), end(lines), string{""});
-  }
+}
 }
 
 string create(char c)
 {
     using namespace impl;
-    return join(append_reversed(double_letters(letters(num_of_letters(c)))));
+    return join(append_reversed(insert_spaces(letters(num_of_letters(c)))));
 }
 }
 
@@ -67,15 +80,45 @@ BOOST_AUTO_TEST_SUITE(test_diamond)
 BOOST_AUTO_TEST_CASE(test_create)
 {
     BOOST_TEST(diamond::create('A') == "A");
-    BOOST_TEST(diamond::create('B') == "ABBA");
-    BOOST_TEST(diamond::create('C') == "ABBCCBBA");
+    BOOST_TEST(diamond::create('B') ==
+	       " A "
+	       "B B"
+	       " A ");
+    BOOST_TEST(diamond::create('C') ==
+	       "  A  "
+	       " B B "
+	       "C   C"
+	       " B B "
+	       "  A  ");
 }
 
-BOOST_AUTO_TEST_CASE(test_double_letters)
+BOOST_AUTO_TEST_CASE(test_insert_spaces)
 {
-  BOOST_TEST(diamond::impl::double_letters("A")   == (vector<string>{"A"}));
-  BOOST_TEST(diamond::impl::double_letters("AB")  == (vector<string>{"A", "BB"}));
-  BOOST_TEST(diamond::impl::double_letters("ABC") == (vector<string>{"A", "BB", "CC"}));
+    using namespace diamond::impl;
+    {
+        const auto input{vector<string>{"A"}};
+        const auto expected{vector<string>{"A"}};
+        const auto actual{insert_spaces(input)};
+        BOOST_CHECK_EQUAL_COLLECTIONS(
+            begin(expected), end(expected),
+            begin(actual), end(actual));
+    }
+    {
+        const auto input{vector<string> {"A", "B"}};
+        const auto expected{vector<string> {" A ", "B B"}};
+        const auto actual{insert_spaces(input)};
+        BOOST_CHECK_EQUAL_COLLECTIONS(
+            begin(expected), end(expected),
+            begin(actual), end(actual));
+    }
+    {
+        const auto input{vector<string> {"A", "B", "C"}};
+        const auto expected{vector<string> {"  A  ", " B B ", "C   C"}};
+        const auto actual{insert_spaces(input)};
+        BOOST_CHECK_EQUAL_COLLECTIONS(
+            begin(expected), end(expected),
+            begin(actual), end(actual));
+    }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
