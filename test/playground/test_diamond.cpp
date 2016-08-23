d16e1fb13fb9c07158652d4b531e1209cd3d6aec
XzoRit <xzorit@hotmail.com>
2016-08-23 08:29:48 +0200

    create char seq from char

--- test/playground/test_diamond.cpp
+++ test/playground/test_diamond.cpp
@@ -1,10 +1,47 @@
+#include <string>
+
+using namespace std::literals::string_literals;
+
+namespace diamond
+{
+namespace impl
+{
+int amount_from_char(char c);
+std::string letters(int amount);
+}
+std::string create(char c);
+}
+
+namespace diamond
+{
+namespace impl
+{
+int amount_from_char(char c)
+{
+  return c - 'A' + 1;
+}
+
+std::string letters(int amount)
+{
+  return "ABCDEFGHIJKLMNOPQRSTUVWXYZ"s.substr(0, amount);
+}
+}
+std::string create(char c)
+{
+  using namespace impl;
+  return letters(amount_from_char(c));
+}
+}
+
 #include <boost/test/unit_test.hpp>
 
 BOOST_AUTO_TEST_SUITE(diamond)
 
-BOOST_AUTO_TEST_CASE(first)
+BOOST_AUTO_TEST_CASE(test_diamond)
 {
-  BOOST_TEST(true);
+  BOOST_TEST(diamond::create('A') == "A");
+  BOOST_TEST(diamond::create('B') == "AB");
+  BOOST_TEST(diamond::create('C') == "ABC");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
