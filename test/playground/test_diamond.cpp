d6bbedb3e6bf80e60c0d79b736b7ca7d8e50779f
XzoRit <xzorit@hotmail.com>
2016-08-23 13:08:42 +0200

    chars are doubled now

--- test/playground/test_diamond.cpp
+++ test/playground/test_diamond.cpp
@@ -10,6 +10,8 @@ string create(char c);
 
 #include <algorithm>
 #include <iterator>
+#include <vector>
+#include <numeric>
 
 namespace diamond
 {
@@ -25,27 +27,55 @@ auto letters(int num)
     return "ABCDEFGHIJKLMNOPQRSTUVWXYZ"s.substr(0, num);
 }
 
-auto append_reversed(const std::string letters)
+  auto append_reversed(const vector<string>& letters)
 {
-  return letters + string{next(rbegin(letters)), rend(letters)};
+  auto reversed{vector<string>(next(rbegin(letters)), rend(letters))};
+  reversed.insert(begin(reversed), begin(letters), end(letters));
+  return reversed;
 }
+
+  auto double_letters(const std::string& letters)
+{
+    auto doubled{vector<string>{"A"}};
+    if(letters.size() == 1) return doubled;
+    for(auto&& c : string{next(begin(letters)), end(letters)})
+    {
+      doubled.push_back(string(2, c));
+    }
+    return doubled;
 }
+
+  auto join(const vector<string>& lines)
+  {
+    return accumulate(begin(lines), end(lines), string{""});
+  }
+}
+
 string create(char c)
 {
     using namespace impl;
-    return append_reversed(letters(num_of_letters(c)));
+    return join(append_reversed(double_letters(letters(num_of_letters(c)))));
 }
 }
 
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_SUITE(diamond)
+namespace utf = boost::unit_test;
 
-BOOST_AUTO_TEST_CASE(test_diamond)
+BOOST_AUTO_TEST_SUITE(test_diamond)
+
+BOOST_AUTO_TEST_CASE(test_create)
 {
     BOOST_TEST(diamond::create('A') == "A");
-    BOOST_TEST(diamond::create('B') == "ABA");
-    BOOST_TEST(diamond::create('C') == "ABCBA");
+    BOOST_TEST(diamond::create('B') == "ABBA");
+    BOOST_TEST(diamond::create('C') == "ABBCCBBA");
+}
+
+BOOST_AUTO_TEST_CASE(test_double_letters)
+{
+  BOOST_TEST(diamond::impl::double_letters("A")   == (vector<string>{"A"}));
+  BOOST_TEST(diamond::impl::double_letters("AB")  == (vector<string>{"A", "BB"}));
+  BOOST_TEST(diamond::impl::double_letters("ABC") == (vector<string>{"A", "BB", "CC"}));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
