0b949f57fa8feb2762281956794bc841313b0da6
XzoRit <xzorit@hotmail.com>
2016-10-12 19:39:13 +0200

    build example depence on to boost program option

--- build/hello/hello.cpp
+++ build/hello/hello.cpp
@@ -1,9 +1,21 @@
-#include <calculator.hpp>
+#include "calculator.hpp"
+#include <boost/program_options.hpp>
 #include <cassert>
+
+namespace po = boost::program_options;
 
 int main(int argc, char *argv[])
 {
-  assert(Calculator::add("1,22,333") == 356);
-  
-  return 0;
+    po::options_description desc("Allowed options");
+    desc.add_options()
+    ("help", "produce help message")
+    ("compression", po::value<int>(), "set compression level");
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    assert(Calculator::add("1,22,333") == 356);
+
+    return 0;
 }
