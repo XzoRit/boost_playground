9b6aba1bdc7a5ebaf4c0f281ef51740c71d263a4
XzoRit <xzorit@hotmail.com>
2016-10-12 21:00:14 +0200

    building an object tree in one line of code

--- di/playground/main.cpp
+++ di/playground/main.cpp
@@ -1,6 +1,53 @@
 #include <boost/di.hpp>
+#include <string>
+#include <iostream>
+
+using namespace std;
+namespace di = boost::di;
+
+struct renderer
+{
+  int device;
+};
+
+class view
+{
+public:
+  view(const std::string& title, const renderer& r)
+  {}
+};
+
+class model
+{};
+
+class controller
+{
+public:
+  controller(model& m, view& v)
+  {}
+};
+
+class user
+{};
+
+class app
+{
+public:
+  app(controller& c, user& u)
+  {}
+
+  void run()
+  {
+    cout << "app::run()\n";
+  }
+};
 
 int main()
 {
+  auto injector = di::make_injector();
+  auto a = injector.create<app>();
+
+  a.run();
+  
   return 0;
 }
