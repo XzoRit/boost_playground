85ec8dd48d823da4ec4d6905adaf5fe6d57858a7
XzoRit <xzorit@hotmail.com>
2017-01-22 15:01:57 +0100

    add playground for boost.coroutine

---
+++ coroutine/main.cpp
@@ -0,0 +1,31 @@
+#include <boost/coroutine/all.hpp>
+#include <boost/bind.hpp>
+#include <iostream>
+#include <functional>
+
+using namespace std;
+namespace bc = boost::coroutines;
+typedef bc::asymmetric_coroutine<int>::pull_type pull_type;
+typedef bc::asymmetric_coroutine<int>::push_type push_type;
+
+void range(push_type& sink, int limit)
+{
+  for(int i = 0; i < limit; ++i)
+    {
+      sink(i);
+    }
+}
+
+pull_type rangex(boost::bind(range, ::_1, 17));
+
+int main()
+{
+  cout << "coroutine\n";
+  for(pull_type::iterator it = bc::begin(rangex);
+      it !=  bc::end(rangex);
+      ++it)
+    {
+      cout << (*it) << '\n';
+    }
+  return 0;
+}
