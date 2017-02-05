64770cd8f54e961ac8a9d901d3d97906beee3adf
XzoRit <xzorit@hotmail.com>
2017-02-05 15:38:14 +0100

    formatted code

--- coroutine/main.cpp
+++ coroutine/main.cpp
@@ -11,21 +11,22 @@ typedef bc::asymmetric_coroutine<int>::push_type push_type;
 void range(push_type& sink, int limit)
 {
   for(int i = 0; i < limit; ++i)
-    {
-      sink(i);
-    }
+  {
+    sink(i);
+  }
 }
 
 pull_type rangex(boost::bind(range, ::_1, 17));
 
 int main()
 {
-  cout << "coroutine\n";
+  cout << "start of coroutine playground\n";
   for(pull_type::iterator it = bc::begin(rangex);
       it !=  bc::end(rangex);
       ++it)
-    {
-      cout << (*it) << '\n';
-    }
+  {
+    cout << (*it) << '\n';
+  }
+  cout << "\nend of coroutine playground\n";
   return 0;
 }
