2092aa2961a23321d9cb3e6fc9d871260269a154
XzoRit <xzorit@hotmail.com>
2017-02-12 11:26:05 +0100

    calc smalles/largest sizeof typelist

--- hana/playground/main.cpp
+++ hana/playground/main.cpp
@@ -17,17 +17,22 @@ auto smallest = hana::minimum(
 template<class ...T>
 using smallest_t = typename decltype(smallest<T...>)::type;
 
+template<class ...T>
+constexpr auto smallest_v = sizeof(smallest_t<T...>);
 
 template<class ...T>
 auto largest = hana::maximum(
-    hana::make_tuple(hana::type_c<T>...),
-    [](auto a, auto b)
+                 hana::make_tuple(hana::type_c<T>...),
+                 [](auto a, auto b)
 {
   return hana::sizeof_(a) < hana::sizeof_(b);
 });
 
 template<class ...T>
 using largest_t = typename decltype(largest<T...>)::type;
+
+template<class ...T>
+constexpr auto largest_v = sizeof(largest_t<T...>);
 
 
 TEST_CASE("type with smallest size")
@@ -36,6 +41,10 @@ TEST_CASE("type with smallest size")
     std::is_same <
     smallest_t<int, long long, char, short>,
     char >::value, "");
+
+  static_assert(
+    smallest_v<int, long long, char, short> == 1,
+    "");
 }
 
 TEST_CASE("type of largest size")
@@ -44,4 +53,8 @@ TEST_CASE("type of largest size")
     std::is_same <
     largest_t<int, long long, char, short>,
     long long >::value, "");
+
+  static_assert(
+    largest_v<int, long long, char, short> == 8,
+    "");
 }
