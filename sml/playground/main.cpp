c9b3df93d09b21114d6eae10b361d603fc6fb530
XzoRit <xzorit@hotmail.com>
2017-03-06 18:15:57 +0100

    formatting

--- sml/playground/main.cpp
+++ sml/playground/main.cpp
@@ -44,23 +44,23 @@ struct Finished {};
 
 struct sendStart
 {
-    void operator()(const Start&){}
+    void operator()(const Start&) {}
 };
 
 struct sendRunning
 {
-    void operator()(const Run&){}
+    void operator()(const Run&) {}
 };
 
 struct sendFinished
 {
-    void operator()(const Finished&){}
+    void operator()(const Finished&) {}
 };
 
-class Hidden{};
-class Idle{};
-class Starting{};
-class Running{};
+class Hidden {};
+class Idle {};
+class Starting {};
+class Running {};
 
 template<class T>
 struct EntryAction
@@ -89,14 +89,14 @@ struct Visible
             make_transition_table
             (
                 *state<Idle> + event<Start> / sendStart{} = state<Starting>
-                ,state<Idle> + on_entry<_> / EntryAction<Idle>{}
-                ,state<Idle> + on_exit<_> / ExitAction<Idle>{}
-                ,state<Starting> + event<Run> / sendRunning{} = state<Running>
-                ,state<Starting> + on_entry<_> / EntryAction<Starting>{}
-                ,state<Starting> + on_exit<_> / ExitAction<Starting>{}
-                ,state<Running> + event<Finished> / sendFinished{} = state<Idle>
-                ,state<Running> + on_entry<_> / EntryAction<Running>{}
-                ,state<Running> + on_exit<_> / ExitAction<Running>{}
+                , state<Idle> + on_entry<_> / EntryAction<Idle> {}
+                , state<Idle> + on_exit<_> / ExitAction<Idle> {}
+                , state<Starting> + event<Run> / sendRunning{} = state<Running>
+                , state<Starting> + on_entry<_> / EntryAction<Starting> {}
+                , state<Starting> + on_exit<_> / ExitAction<Starting> {}
+                , state<Running> + event<Finished> / sendFinished{} = state<Idle>
+                , state<Running> + on_entry<_> / EntryAction<Running> {}
+                , state<Running> + on_exit<_> / ExitAction<Running> {}
             );
     }
 };
@@ -110,12 +110,12 @@ struct Controller
             make_transition_table
             (
                 *state<Hidden> + event<Show> = state<Visible>
-                ,state<Hidden> + event<Start> = state<Starting>
-                ,state<Hidden> + on_entry<_> / EntryAction<Hidden>{}
-                ,state<Hidden> + on_exit<_> / ExitAction<Hidden>{}
-                ,state<Visible> + event<Hide> = state<Hidden>
-                ,state<Visible> + on_entry<_> / EntryAction<Visible>{}
-                ,state<Visible> + on_exit<_> / ExitAction<Visible>{}
+                , state<Hidden> + event<Start> = state<Starting>
+                , state<Hidden> + on_entry<_> / EntryAction<Hidden> {}
+                , state<Hidden> + on_exit<_> / ExitAction<Hidden> {}
+                , state<Visible> + event<Hide> = state<Hidden>
+                , state<Visible> + on_entry<_> / EntryAction<Visible> {}
+                , state<Visible> + on_exit<_> / ExitAction<Visible> {}
             );
     }
 };
@@ -123,53 +123,65 @@ struct Controller
 #include <cassert>
 
 template <class T>
-void dump_transition() noexcept {
-  auto src_state = std::string{sml::aux::string<typename T::src_state>{}.c_str()};
-  auto dst_state = std::string{sml::aux::string<typename T::dst_state>{}.c_str()};
-  if (dst_state == "X") {
-    dst_state = "[*]";
-  }
+void dump_transition() noexcept
+{
+    auto src_state = std::string{sml::aux::string<typename T::src_state>{} .c_str()};
+    auto dst_state = std::string{sml::aux::string<typename T::dst_state>{} .c_str()};
+    if(dst_state == "X")
+    {
+        dst_state = "[*]";
+    }
 
-  if (T::initial) {
-    std::cout << "[*] --> " << src_state << std::endl;
-  }
+    if(T::initial)
+    {
+        std::cout << "[*] --> " << src_state << std::endl;
+    }
 
-  std::cout << src_state << " --> " << dst_state;
+    std::cout << src_state << " --> " << dst_state;
 
-  const auto has_event = !sml::aux::is_same<typename T::event, sml::anonymous>::value;
-  const auto has_guard = !sml::aux::is_same<typename T::guard, sml::front::always>::value;
-  const auto has_action = !sml::aux::is_same<typename T::action, sml::front::none>::value;
+    const auto has_event =
+        !sml::aux::is_same<typename T::event, sml::anonymous>::value;
+    const auto has_guard =
+        !sml::aux::is_same<typename T::guard, sml::front::always>::value;
+    const auto has_action =
+        !sml::aux::is_same<typename T::action, sml::front::none>::value;
 
-  if (has_event || has_guard || has_action) {
-    std::cout << " :";
-  }
+    if(has_event || has_guard || has_action)
+    {
+        std::cout << " :";
+    }
 
-  if (has_event) {
-    std::cout << " " << typeid(typename T::event).name();
-  }
+    if(has_event)
+    {
+        std::cout << " " << typeid(typename T::event).name();
+    }
 
-  if (has_guard) {
-    std::cout << " [" << typeid(typename T::guard).name() << "]";
-  }
+    if(has_guard)
+    {
+        std::cout << " [" << typeid(typename T::guard).name() << "]";
+    }
 
-  if (has_action) {
-    std::cout << " / " << typeid(typename T::action).name();
-  }
+    if(has_action)
+    {
+        std::cout << " / " << typeid(typename T::action).name();
+    }
 
-  std::cout << std::endl;
+    std::cout << std::endl;
 }
 
 template <template <class...> class T, class... Ts>
-void dump_transitions(const T<Ts...>&) noexcept {
-  int _[] = {0, (dump_transition<Ts>(), 0)...};
-  (void)_;
+void dump_transitions(const T<Ts...>&) noexcept
+{
+    int _[] = {0, (dump_transition<Ts>(), 0)...};
+    (void)_;
 }
 
 template <class SM>
-void dump(const SM&) noexcept {
-  std::cout << "@startuml" << std::endl << std::endl;
-  dump_transitions(typename SM::transitions{});
-  std::cout << std::endl << "@enduml" << std::endl;
+void dump(const SM&) noexcept
+{
+    std::cout << "@startuml" << std::endl << std::endl;
+    dump_transitions(typename SM::transitions{});
+    std::cout << std::endl << "@enduml" << std::endl;
 }
 
 int main()
