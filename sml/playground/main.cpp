77c80cccdc77f673e3851f52a0178569ea8fc46b
XzoRit <xzorit@hotmail.com>
2017-03-06 18:13:43 +0100

    state machine visitor

--- sml/playground/main.cpp
+++ sml/playground/main.cpp
@@ -122,11 +122,63 @@ struct Controller
 
 #include <cassert>
 
+template <class T>
+void dump_transition() noexcept {
+  auto src_state = std::string{sml::aux::string<typename T::src_state>{}.c_str()};
+  auto dst_state = std::string{sml::aux::string<typename T::dst_state>{}.c_str()};
+  if (dst_state == "X") {
+    dst_state = "[*]";
+  }
+
+  if (T::initial) {
+    std::cout << "[*] --> " << src_state << std::endl;
+  }
+
+  std::cout << src_state << " --> " << dst_state;
+
+  const auto has_event = !sml::aux::is_same<typename T::event, sml::anonymous>::value;
+  const auto has_guard = !sml::aux::is_same<typename T::guard, sml::front::always>::value;
+  const auto has_action = !sml::aux::is_same<typename T::action, sml::front::none>::value;
+
+  if (has_event || has_guard || has_action) {
+    std::cout << " :";
+  }
+
+  if (has_event) {
+    std::cout << " " << typeid(typename T::event).name();
+  }
+
+  if (has_guard) {
+    std::cout << " [" << typeid(typename T::guard).name() << "]";
+  }
+
+  if (has_action) {
+    std::cout << " / " << typeid(typename T::action).name();
+  }
+
+  std::cout << std::endl;
+}
+
+template <template <class...> class T, class... Ts>
+void dump_transitions(const T<Ts...>&) noexcept {
+  int _[] = {0, (dump_transition<Ts>(), 0)...};
+  (void)_;
+}
+
+template <class SM>
+void dump(const SM&) noexcept {
+  std::cout << "@startuml" << std::endl << std::endl;
+  dump_transitions(typename SM::transitions{});
+  std::cout << std::endl << "@enduml" << std::endl;
+}
+
 int main()
 {
     using namespace sml;
     PrintfLogger l;
     sm<Controller, logger<PrintfLogger>> sm{l};
+    dump(sm);
+
     assert(sm.is<decltype(state<Controller>)>(state<Hidden>));
     assert(sm.is<decltype(state<Visible>)>(state<Idle>));
 
