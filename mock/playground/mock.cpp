812a16c00cb5be38e33e5eea3af072b77977cbb2
XzoRit <xzorit@hotmail.com>
2017-03-08 08:44:39 +0100

    mock with sequences and return action

--- mock/playground/mock.cpp
+++ mock/playground/mock.cpp
@@ -4,7 +4,7 @@
 class IView
 {
 public:
-    virtual void display(int) const = 0;
+    virtual bool display(int) const = 0;
 };
 
 class Calculator
@@ -24,7 +24,7 @@ private:
 
 MOCK_BASE_CLASS(MockView, IView)
 {
-    MOCK_CONST_METHOD(display, 1, void(int));
+    MOCK_CONST_METHOD(display, 1, bool(int));
 };
 
 BOOST_AUTO_TEST_CASE(test_1)
@@ -32,10 +32,13 @@ BOOST_AUTO_TEST_CASE(test_1)
     MockView v;
     Calculator c(v);
 
-    mock::sequence s;
-    MOCK_EXPECT(v.display).once().with(0).in(s);
-    MOCK_EXPECT(v.display).once().with(1).in(s);
+    mock::sequence s1, s2;
+    MOCK_EXPECT(v.display).once().with(0).in(s1)    .returns(true);
+    MOCK_EXPECT(v.display).once().with(1).in(s2)    .returns(true);
+    MOCK_EXPECT(v.display)       .with(2).in(s1, s2).returns(true);
 
    c.add(0, 0);
    c.add(1, 0);
+   c.add(1, 1);
+   c.add(0, 2);
 }
