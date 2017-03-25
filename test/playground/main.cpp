775bc81aa57253259442ab7349e6ea597f30a0bf
XzoRit <xzorit@hotmail.com>
2017-03-25 10:16:23 +0100

    remove hint concerning comandline arguments

--- test/playground/main.cpp
+++ test/playground/main.cpp
@@ -1,3 +1,2 @@
-/* ../../../boost/b2 -sBOOST_ROOT=../../../boost && ../bin/test_playground --log_level=message */
 #define BOOST_TEST_MODULE test_playground
 #include <boost/test/included/unit_test.hpp>
