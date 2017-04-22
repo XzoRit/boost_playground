c4df8de29b4e85d2b1f7c4e0b25b719b6c648444
XzoRit <xzorit@hotmail.com>
2017-04-22 17:09:47 +0200

    starting to build a restorer type

--- test/playground/test_fixtures.cpp
+++ test/playground/test_fixtures.cpp
@@ -75,3 +75,37 @@ BOOST_AUTO_TEST_CASE(lambda_teardown_check)
 }
 
 BOOST_AUTO_TEST_SUITE_END()
+
+template<class... T>
+class restorer
+{
+public:
+    restorer(T... ts)
+        : m_values{std::make_tuple(ts...)}
+    {
+    }
+private:
+    std::tuple<T...> m_values;
+};
+
+BOOST_AUTO_TEST_SUITE(restorer_suite)
+
+BOOST_AUTO_TEST_CASE(ctor_takes_one_parameter)
+{
+    using ints = std::vector<int>;
+    auto is = ints{};
+    auto r = restorer<ints>{is};
+}
+
+BOOST_AUTO_TEST_CASE(ctor_takes_many_parameters)
+{
+    using ints            = std::vector<int             >;
+    using strings         = std::list  <std::string     >;
+    using ints_to_strings = std::map   <int, std::string>;
+    auto is       = ints           {};
+    auto ss       = strings        {};
+    auto is_to_ss = ints_to_strings{};
+    auto r = restorer<ints, strings, ints_to_strings>{is, ss, is_to_ss};
+}
+
+BOOST_AUTO_TEST_SUITE_END()
