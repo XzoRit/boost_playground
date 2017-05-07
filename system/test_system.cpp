9c34595b9ff10035cafaffeeced61f9331bce25d
XzoRit <xzorit@hotmail.com>
2017-05-07 15:53:43 +0200

    error_code is implicitly true

--- system/test_system.cpp
+++ system/test_system.cpp
@@ -1,10 +1,22 @@
 #include <boost/test/unit_test.hpp>
+#include <boost/system/error_code.hpp>
+
+using namespace boost::system;
 
 BOOST_AUTO_TEST_SUITE(boost_system)
 
-BOOST_AUTO_TEST_CASE(test_1)
+void not_ok(error_code& ec)
 {
-    BOOST_REQUIRE(true);
+    ec = errc::make_error_code(errc::not_supported);
+}
+
+BOOST_AUTO_TEST_CASE(error_code_is_implicitly_true)
+{
+    error_code ec{};
+
+    not_ok(ec);
+
+    BOOST_REQUIRE(ec);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
