51e119c5bc45125654409d0501ec74890573dcb1
XzoRit <xzorit@hotmail.com>
2017-05-07 18:26:37 +0200

    verify error code is from category generic

--- system/test_system.cpp
+++ system/test_system.cpp
@@ -1,28 +1,53 @@
 #include <boost/test/unit_test.hpp>
+#define BOOST_SYSTEM_NO_DEPRECATED
 #include <boost/system/error_code.hpp>
+
+#include <ostream>
+
+namespace boost::system
+{
+    std::ostream& boost_test_print_type(std::ostream& str, const error_category& ec)
+    {
+        str << ec.name();
+        return str;
+    }
+}
 
 using namespace boost::system;
 
-BOOST_AUTO_TEST_SUITE(boost_system)
+class fixture
+{
+public:
+    fixture() {}
+    ~fixture(){}
+
+    error_code ec{};
+};
 
 void not_ok(error_code& ec)
 {
     ec = errc::make_error_code(errc::not_supported);
 }
 
-BOOST_AUTO_TEST_CASE(default_constructed_error_code_is_no_error)
+BOOST_AUTO_TEST_SUITE(boost_system)
+
+BOOST_FIXTURE_TEST_CASE(default_constructed_error_code_is_no_error, fixture)
 {
-    error_code ec{};
     BOOST_REQUIRE(!ec);
 }
 
-BOOST_AUTO_TEST_CASE(set_error_code_is_implicitly_true)
+BOOST_FIXTURE_TEST_CASE(set_error_code_is_implicitly_true, fixture)
 {
-    error_code ec{};
-
     not_ok(ec);
 
     BOOST_REQUIRE(ec);
+}
+
+BOOST_FIXTURE_TEST_CASE(error_codes_catagory_by_default_is_generic, fixture)
+{
+    not_ok(ec);
+
+    BOOST_REQUIRE_EQUAL(ec.category(), generic_category());
 }
 
 BOOST_AUTO_TEST_SUITE_END()
