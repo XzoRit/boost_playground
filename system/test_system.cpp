8f6a2804c9bb493b7a35f832a92a042c99009c19
XzoRit <xzorit@hotmail.com>
2017-05-09 19:54:39 +0200

    test creation of own error codes

--- system/test_system.cpp
+++ system/test_system.cpp
@@ -16,11 +16,11 @@ namespace boost::system
 using namespace std::string_literals;
 using namespace boost::system;
 
-class fixture
+class boost_system_fixture
 {
 public:
-    fixture() {}
-    ~fixture(){}
+    boost_system_fixture() {}
+    ~boost_system_fixture(){}
 
     error_code ec{};
 };
@@ -32,20 +32,20 @@ void not_ok(error_code& ec)
 
 BOOST_AUTO_TEST_SUITE(boost_system)
 
-BOOST_FIXTURE_TEST_CASE(default_constructed_error_code_is_no_error, fixture)
+BOOST_FIXTURE_TEST_CASE(default_constructed_error_code_is_no_error, boost_system_fixture)
 {
     BOOST_REQUIRE(!ec);
     BOOST_REQUIRE_EQUAL(ec, (error_code{0, system_category()}));
 }
 
-BOOST_FIXTURE_TEST_CASE(set_error_code_is_implicitly_true, fixture)
+BOOST_FIXTURE_TEST_CASE(set_error_code_is_implicitly_true, boost_system_fixture)
 {
     not_ok(ec);
 
     BOOST_REQUIRE(ec);
 }
 
-BOOST_FIXTURE_TEST_CASE(error_codes_catagory_by_default_is_generic, fixture)
+BOOST_FIXTURE_TEST_CASE(error_codes_catagory_by_default_is_generic, boost_system_fixture)
 {
     not_ok(ec);
 
@@ -64,14 +64,14 @@ class app_cat_t : public error_category
 {
 public:
     const char* name() const noexcept override
-        {
-            return "app";
-        }
+    {
+        return "app";
+    }
 
     std::string message(int ev) const noexcept override
-        {
-            return "app error: "s + std::to_string(ev);
-        }
+    {
+        return "app error: "s + std::to_string(ev);
+    }
 };
 
 const app_cat_t app_cat;
@@ -81,13 +81,66 @@ error_code make_error_code(app_errc ec)
     return error_code(static_cast<int>(ec), app_cat);
 }
 
-BOOST_AUTO_TEST_SUITE(app_error_code)
-
-BOOST_AUTO_TEST_CASE(app_errc_construction)
+enum class lib_errc
 {
-    error_code ec{make_error_code(app_errc::success)};
+    success = 0,
+    failure
+};
 
-    BOOST_REQUIRE(!ec);
+class lib_cat_t : public error_category
+{
+public:
+    const char* name() const noexcept override
+    {
+        return "lib";
+    }
+
+    std::string message(int ev) const noexcept override
+    {
+        return "lib error: "s + std::to_string(ev);
+    }
+};
+
+const lib_cat_t lib_cat;
+
+error_code make_error_code(lib_errc ec)
+{
+    return error_code(static_cast<int>(ec), lib_cat);
+}
+
+class my_error_code_fixture
+{
+public:
+    my_error_code_fixture() {}
+    ~my_error_code_fixture(){}
+
+    error_code ec_app_succ{make_error_code(app_errc::success)};
+    error_code ec_app_fail{make_error_code(app_errc::failure)};
+};
+
+BOOST_AUTO_TEST_SUITE(my_error_code)
+
+BOOST_FIXTURE_TEST_CASE(my_errc_construction, my_error_code_fixture)
+{
+    BOOST_REQUIRE(!ec_app_succ);
+    BOOST_REQUIRE( ec_app_fail);
+    BOOST_REQUIRE_EQUAL(ec_app_succ.message(), "app error: 0");
+    BOOST_REQUIRE_EQUAL(ec_app_fail.message(), "app error: 1");
+
+    BOOST_REQUIRE_EQUAL(ec_app_succ, make_error_code(app_errc::success));
+    BOOST_REQUIRE_EQUAL(ec_app_fail, make_error_code(app_errc::failure));
+    BOOST_REQUIRE_NE(ec_app_succ, ec_app_fail);
+}
+
+BOOST_FIXTURE_TEST_CASE(my_errc_comparison, my_error_code_fixture)
+{
+    error_code ec_lib_succ{make_error_code(lib_errc::success)};
+    error_code ec_lib_fail{make_error_code(lib_errc::failure)};
+
+    BOOST_REQUIRE_NE(ec_lib_succ, ec_app_succ);
+    BOOST_REQUIRE_NE(ec_lib_succ, ec_app_fail);
+    BOOST_REQUIRE_NE(ec_lib_fail, ec_app_succ);
+    BOOST_REQUIRE_NE(ec_lib_fail, ec_app_fail);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
