4497b78ac2dfa8f8976d6a2443acd6b7044afa29
XzoRit <xzorit@hotmail.com>
2017-06-05 12:03:31 +0200

    move tests for own error codes into own cpp file

--- system/test_system.cpp
+++ system/test_system.cpp
@@ -1,6 +1,3 @@
-#include "app_error_codes.hpp"
-#include "lib_error_codes.hpp"
-
 #include <boost/test/unit_test.hpp>
 
 #define BOOST_SYSTEM_NO_DEPRECATED
@@ -55,55 +52,6 @@ BOOST_FIXTURE_TEST_CASE(error_codes_catagory_by_default_is_generic, boost_system
     not_ok(ec);
 
     BOOST_REQUIRE_EQUAL(ec.category(), generic_category());
-}
-
-BOOST_AUTO_TEST_SUITE_END()
-
-class my_error_code_fixture
-{
-public:
-    my_error_code_fixture() {}
-    ~my_error_code_fixture(){}
-};
-
-BOOST_AUTO_TEST_SUITE(my_error_code)
-
-BOOST_FIXTURE_TEST_CASE(my_errc_construction, my_error_code_fixture)
-{
-    BOOST_REQUIRE(!app::ec_succ);
-    BOOST_REQUIRE( app::ec_fail);
-    BOOST_REQUIRE_EQUAL(app::ec_succ.message(), "app error: 0");
-    BOOST_REQUIRE_EQUAL(app::ec_fail.message(), "app error: 1");
-}
-
-BOOST_FIXTURE_TEST_CASE(my_errc_comparison, my_error_code_fixture)
-{
-    BOOST_REQUIRE_EQUAL(app::ec_succ, make_error_code(app::error::success));
-    BOOST_REQUIRE_EQUAL(app::ec_fail, make_error_code(app::error::failure));
-    BOOST_REQUIRE_NE(app::ec_succ, app::ec_fail);
-
-    BOOST_REQUIRE_NE(lib::ec_succ, app::ec_succ);
-    BOOST_REQUIRE_NE(lib::ec_succ, app::ec_fail);
-    BOOST_REQUIRE_NE(lib::ec_fail, app::ec_succ);
-    BOOST_REQUIRE_NE(lib::ec_fail, app::ec_fail);
-}
-
-BOOST_FIXTURE_TEST_CASE(my_errcs_are_recognized_by_stl, my_error_code_fixture)
-{
-    static_assert(is_error_code_enum<app::error::type>::value, "");
-    static_assert(is_error_code_enum<lib::error::type>::value, "");
-}
-
-BOOST_FIXTURE_TEST_CASE(my_errcs_can_be_thrown_as_system_error, my_error_code_fixture)
-{
-    BOOST_REQUIRE_EXCEPTION(
-        throw system_error(app::ec_fail),
-        system_error,
-        [](const auto& e){ return e.code() == app::ec_fail; });
-    BOOST_REQUIRE_EXCEPTION(
-        throw system_error(lib::ec_fail),
-        system_error,
-        [](const auto& e){ return e.code() == lib::ec_fail; });
 }
 
 BOOST_AUTO_TEST_SUITE_END()
