e2042c891915376871eff1fc628a2f0898f23690
XzoRit <xzorit@hotmail.com>
2018-04-17 21:08:44 +0200

    add new project to learn how cool boost.hof is

---
+++ hof/playground/test_hof.cpp
@@ -0,0 +1,14 @@
+#include <boost/test/unit_test.hpp>
+
+using namespace std;
+namespace utf = boost::unit_test;
+namespace tt  = boost::test_tools;
+
+BOOST_AUTO_TEST_SUITE(hof)
+
+BOOST_AUTO_TEST_CASE(sum_function_as_first_class_citizen)
+{
+    BOOST_TEST(false);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
