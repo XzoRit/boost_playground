f018f6c3e2fcbb38c615fe4e42dfc32fc6837f02
XzoRit <xzorit@hotmail.com>
2021-11-18 14:48:31 +0100

    combine boost.describe with crtp

--- describe/test_describe.cpp
+++ describe/test_describe.cpp
@@ -1,10 +1,139 @@
+#include <boost/describe.hpp>
+#include <boost/mp11.hpp>
 #include <boost/test/unit_test.hpp>
+
+#include <ostream>
+#include <sstream>
+
+using namespace boost::describe;
+
+namespace v1
+{
+template <class T, class Bd = describe_bases<T, mod_any_access>, class Md = describe_members<T, mod_any_access>>
+std::ostream& operator<<(std::ostream& os, T const& t)
+{
+    os << "{";
+
+    bool first = true;
+
+    boost::mp11::mp_for_each<Bd>([&](auto D) {
+        if (!first)
+        {
+            os << ", ";
+        }
+        first = false;
+
+        using B = typename decltype(D)::type;
+        os << (B const&)t;
+    });
+
+    boost::mp11::mp_for_each<Md>([&](auto D) {
+        if (!first)
+        {
+            os << ", ";
+        }
+        first = false;
+
+        os << "." << D.name << " = " << t.*D.pointer;
+    });
+
+    os << "}";
+    return os;
+}
+
+struct X
+{
+    int m1 = 1;
+};
+
+BOOST_DESCRIBE_STRUCT(X, (), (m1))
+
+struct Y
+{
+    int m2 = 2;
+};
+
+BOOST_DESCRIBE_STRUCT(Y, (), (m2))
+
+class Z : public X, private Y
+{
+    int m1 = 3;
+    int m2 = 4;
+
+    BOOST_DESCRIBE_CLASS(Z, (X, Y), (), (), (m1, m2))
+};
+} // namespace v1
+
+namespace v2
+{
+template <class T>
+struct ostreamable
+{
+};
+
+template <class T, class Md = describe_members<T, mod_any_access>>
+inline std::ostream& operator<<(std::ostream& os, const ostreamable<T>& o)
+{
+    const T& t{*static_cast<const T*>(&o)};
+
+    os << "{ ";
+
+    boost::mp11::mp_for_each<Md>([&](auto D) { os << '.' << D.name << '=' << t.*D.pointer << ' '; });
+
+    os << "}";
+
+    return os;
+}
+
+struct X : ostreamable<X>
+{
+    int m1 = 1;
+};
+
+BOOST_DESCRIBE_STRUCT(X, (), (m1))
+
+struct Y : ostreamable<Y>
+{
+    int m2 = 2;
+};
+
+BOOST_DESCRIBE_STRUCT(Y, (), (m2))
+
+class Z : public ostreamable<Z>
+{
+    int m1 = 3;
+    int m2 = 4;
+
+    BOOST_DESCRIBE_CLASS(Z, (), (), (), (m1, m2))
+};
+} // namespace v2
 
 BOOST_AUTO_TEST_SUITE(boost_describe)
 
-BOOST_AUTO_TEST_CASE(valid)
+BOOST_AUTO_TEST_CASE(ostream_for_types_in_same_namespace)
 {
-    BOOST_CHECK(false);
+    std::ostringstream str{};
+    str << v1::Z{};
+    BOOST_CHECK_EQUAL(str.str(), "{{.m1 = 1}, {.m2 = 2}, .m1 = 3, .m2 = 4}");
+}
+
+BOOST_AUTO_TEST_CASE(crtp_ostreamable)
+{
+    {
+        std::ostringstream str{};
+        str << v2::X{};
+        BOOST_CHECK_EQUAL(str.str(), "{ .m1=1 }");
+    }
+    {
+        std::ostringstream str{};
+        str << v2::Y{};
+        BOOST_CHECK_EQUAL(str.str(), "{ .m2=2 }");
+    }
+    {
+        std::ostringstream str{};
+        str << v2::Z{};
+        BOOST_CHECK_EQUAL(str.str(), "{ .m1=3 .m2=4 }");
+    }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
